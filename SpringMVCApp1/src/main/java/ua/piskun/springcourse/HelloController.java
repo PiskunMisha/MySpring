02bd8e15c63854d71c192f05ccac8a1e9c386bda
PiskunMisha <152208325+PiskunMisha@users.noreply.github.com>
2025-06-20 15:04:18 +0300

    cleaning .idea and tagret

---
+++ SpringMVCApp1/src/main/java/ua/piskun/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package ua.piskun.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/h")
+    public String sayHello(){
+        return "h";
+    }
+}
--- springmvc1/src/main/java/ua/piskun/springcourse/HelloController.java
+++ springmvc1/src/main/java/ua/piskun/springcourse/HelloController.java
@@ -1,13 +1,21 @@
 package ua.piskun.springcourse;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HelloController {
 
-    @GetMapping("/hello-world")
-    public String sayHello(){
-        return "hello_world";
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hello_world", urlPatterns = {"/h"})
+public class HelloController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws RuntimeException, ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/hello_world.html");
+        dispatcher.forward(req, resp);
     }
 }
