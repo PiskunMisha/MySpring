6df72f7bff6d0afcbce8c0246a2c1fa13319b13a
PiskunMisha <152208325+PiskunMisha@users.noreply.github.com>
2025-06-23 17:28:44 +0300

    SpringCRUD1-3, and fix

---
+++ springCRUD1/src/main/java/ua/piskun/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ua.piskun.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.piskun.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Misha"));
+        people.add(new Person(++PEOPLE_COUNT, "Valera"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
+    }
+}
