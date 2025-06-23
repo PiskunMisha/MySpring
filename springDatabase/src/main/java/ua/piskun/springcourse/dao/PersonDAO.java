619cfef8b00899ddbef4996543c423bbe403a823
PiskunMisha <152208325+PiskunMisha@users.noreply.github.com>
2025-06-23 21:44:05 +0300

    Spring DB

---
+++ springDatabase/src/main/java/ua/piskun/springcourse/dao/PersonDAO.java
@@ -0,0 +1,45 @@
+package ua.piskun.springcourse.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ua.piskun.springcourse.models.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+       return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("select * from person where id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("insert into person values(1, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail());
+    }
+
+    public void update(int id, Person updatePerson) {
+        jdbcTemplate.update("update person set name=?, age=?, email=? where id=?",
+                updatePerson.getName(), updatePerson.getAge(),  updatePerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("delete from person where id=?", id);
+    }
+}
